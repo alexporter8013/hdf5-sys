61be6693a1102ff3327e71e0a08922afdcc11b2c
bomber8013 <bomber8013@gmail.com>
2019-02-06 01:01:35 -0800

    working now

--- build.rs
+++ build.rs
@@ -1,13 +1,9 @@
-use std::env;
-use std::path::Path;
+//extern crate bindgen;
+
+//use std::path::PathBuf;
+//use std::env;
 
 fn main() {
-	let lib_var = env::var("HDF5_LIB").expect("Could not find HDF5_LIB");
-	let lib_path = Path::new(&lib_var);
-
-	let inc_var = env::var("HDF5_INCLUDE").expect("Could not find HDF5_INCLUDE");
-	let inc_path = Path::new(&inc_var);
-	println!("cargo:include={:?}", inc_path);
-	println!("cargo:rustc-link-search=native={:?}", lib_path);
-	println!("cargo:rustc-link-search=native={:?}", inc_path);
+    println!("cargo:rustc-link-lib=hdf5");
+    
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,14 @@
-//pub fn 
+
+mod bindings;
+pub use bindings::*; 
 
 #[cfg(test)]
 mod tests {
+	use super::*;
     #[test]
-    fn it_works() {
-        H5_open();
+    fn version() {
+        let (mut majnum, mut minnum, mut relnum) = (0, 0, 0);
+        assert!(unsafe { H5get_libversion(&mut majnum, &mut minnum, &mut relnum) } >= 0);
+        assert_eq!((majnum, minnum, relnum), (1, 10, 4));
     }
 }
